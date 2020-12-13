718282d860af2ca1c9e31fc2e7cff9251a64c1d0
Karishma2802 <Karishmaneha2015@gmail.com>
2020-12-13 22:43:17 +0530

    Adding UC2

---
+++ UC2.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+declare -A coin
+declare -A coinPer
+
+totalFlips=200
+count=0
+
+while (( $count < totalFlips ))
+do
+coinFlip=$((RANDOM%4))
+
+case $coinFlip in
+0) echo "HH"
+        if [ -z "${coin[HH]}" ]
+   then
+      coin[HH]=1
+   else
+      coin[HH]=$((${coin[HH]}+1))
+   fi
+;;
+1) echo "HT"
+        if [ -z "${coin[HT]}" ]
+   then
+        coin[HT]=1
+   else
+      coin[HT]=$((${coin[HT]}+1))
+   fi
+;;
+2) echo "TH"
+        if [ -z "${coin[TH]}" ]
+   then
+      coin[TH]=1
+   else
+      coin[TH]=$((${coin[TH]}+1))
+   fi
+;;
+3) echo "TT"
+        if [ -z "${coin[TT]}" ]
+   then
+      coin[TT]=1
+   else
+      coin[TT]=$((${coin[TT]}+1))
+   fi
+;;
+esac
+((count++))
+done
+
+echo "HH/HT/TH/TT: "${!coin[@]}
+echo "Value: "${coin[@]}
+
+HHper=`awk "BEGIN {print (${coin[HH]}/$totalFlips)*100}"`
+HTper=`awk "BEGIN {print (${coin[HT]}/$totalFlips)*100}"`
+THper=`awk "BEGIN {print (${coin[TH]}/$totalFlips)*100}"`
+TTper=`awk "BEGIN {print (${coin[TT]}/$totalFlips)*100}"`
+
+coinPer[HH]=$HHper
+coinPer[HT]=$HTper
+coinPer[TH]=$THper
+coinPer[TT]=$TTper
+
+echo "HH Percentage: $HHper% "
+echo "HT Percentage: $HTper%"
+echo "TH Percentage: $THper%"
+echo "TT Percentage: $TTper%"
+echo "=============="
+
+
+arr=(`printf '%s\n' "${coinPer[@]}" | sort -n`)
+max=${arr[3]}
+
+for key in "${!coinPer[@]}"
+do
+	if [[ ${coinPer[$key]} == $max ]]
+	then
+		echo "Maximum Combinationa is: " "coinPer["$key"]" "Percentage is" $max
+	exit
+	fi
+done
